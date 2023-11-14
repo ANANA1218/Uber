50d9f3cc867cdb77eb44dc588ebb73b594777fc7
Annabelle <annabelle.bosse.pro@gmail.com>
2023-11-14 16:11:49 +0100

    entity et repository

---
+++ src/Entity/Commande.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+use App\Entity\Membre;
+use App\Entity\Vehicule;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(name: "date_heure_depart", type: "datetime")]
+    private \DateTimeInterface $dateHeureDepart;
+
+    #[ORM\Column(name: "date_heure_fin", type: "datetime")]
+    private \DateTimeInterface $dateHeureFin;
+
+    #[ORM\Column(name: "prix_total", type: "integer")]
+    private int $prixTotal;
+
+    #[ORM\Column(name: "date_enregistrement", type: "datetime")]
+    private \DateTimeInterface $dateEnregistrement;
+
+    #[ORM\ManyToOne(targetEntity: Membre::class)]
+    #[ORM\JoinColumn(name: "id_membre", referencedColumnName: "id", nullable: false)]
+    private Membre $membre;
+
+    #[ORM\ManyToOne(targetEntity: Vehicule::class)]
+    #[ORM\JoinColumn(name: "id_vehicule", referencedColumnName: "id", nullable: false)]
+    private Vehicule $vehicule;
+
+    // Les getters et setters pour les nouveaux champs
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateHeureDepart(): \DateTimeInterface
+    {
+        return $this->dateHeureDepart;
+    }
+
+    public function setDateHeureDepart(\DateTimeInterface $dateHeureDepart): self
+    {
+        $this->dateHeureDepart = $dateHeureDepart;
+        return $this;
+    }
+
+    public function getDateHeureFin(): \DateTimeInterface
+    {
+        return $this->dateHeureFin;
+    }
+
+    public function setDateHeureFin(\DateTimeInterface $dateHeureFin): self
+    {
+        $this->dateHeureFin = $dateHeureFin;
+        return $this;
+    }
+
+    public function getPrixTotal(): int
+    {
+        return $this->prixTotal;
+    }
+
+    public function setPrixTotal(int $prixTotal): self
+    {
+        $this->prixTotal = $prixTotal;
+        return $this;
+    }
+
+    public function getDateEnregistrement(): \DateTimeInterface
+    {
+        return $this->dateEnregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeInterface $dateEnregistrement): self
+    {
+        $this->dateEnregistrement = $dateEnregistrement;
+        return $this;
+    }
+
+    public function getMembre(): Membre
+    {
+        return $this->membre;
+    }
+
+    public function setMembre(Membre $membre): self
+    {
+        $this->membre = $membre;
+        return $this;
+    }
+
+    public function getVehicule(): Vehicule
+    {
+        return $this->vehicule;
+    }
+
+    public function setVehicule(Vehicule $vehicule): self
+    {
+        $this->vehicule = $vehicule;
+        return $this;
+    }
+}
---
+++ src/Entity/Membre.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MembreRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MembreRepository::class)]
+class Membre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $mdp = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?bool $civilite = null;
+
+    #[ORM\Column]
+    private ?int $status = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_enregistrement = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): static
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function isCivilite(): ?bool
+    {
+        return $this->civilite;
+    }
+
+    public function setCivilite(bool $civilite): static
+    {
+        $this->civilite = $civilite;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->date_enregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeInterface $date_enregistrement): static
+    {
+        $this->date_enregistrement = $date_enregistrement;
+
+        return $this;
+    }
+}
