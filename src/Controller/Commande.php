c81aa86073cfe5376424f9608040c1a8ce7b0ec4
Annabelle <annabelle.bosse.pro@gmail.com>
2023-11-15 09:45:02 +0100

    bon

--- src/Controller/Commande.php
+++
@@ -1,2 +0,0 @@
-// get all commande pour les mettre en liste 
-
--- src/Controller/HomeController.php
+++
@@ -1,7 +0,0 @@
-// get all vehicule 
-// get by id 
-
-// faire un fonction de recherche selon une date de début et une date de fin
-
-
-//  cette partie est libre pas reserver au membre 
